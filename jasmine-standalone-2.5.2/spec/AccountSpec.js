c4f23b76bb14c263a2199ab629270e06bdcd6f15
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-01-23 11:34:39 +0000

    withdrawals and deposits deduct from balance

--- jasmine-standalone-2.5.2/spec/AccountSpec.js
+++ jasmine-standalone-2.5.2/spec/AccountSpec.js
@@ -5,29 +5,61 @@
 
   beforeEach(function() {
     account = new Account()
+    statement = jasmine.createSpyObj('statement', ['createWithdrawal'])
   })
 
   it("the balance should start at zero", function() {
     expect(account.balance).toEqual(0)
   })
 
+  it("has a statement", function() {
+    expect(account.statement).toBeDefined()
+  })
+
   describe("making a withdrawal", function() {
 
-    it("throws an error if you have insufficient funds", function() {
-      expect(function() {account.withdraw(10)}).toThrow(new Error("You have insufficient funds"))
+    describe("with insufficent funds", function() {
+
+      it("throws an error", function() {
+        expect(function() {account.withdraw(10)}).toThrow(new Error("You have insufficient funds"))
+      })
     })
 
-    it("accepts the withdrawal when there is sufficient funds", function() {
-      account.deposit(11)
-      expect(account.withdraw(10)).toEqual("withdrawal successful")
+    describe("with sufficient funds", function() {
+
+      beforeEach(function() {
+        account.deposit(11);
+      })
+
+      it("accepts the withdrawal when there is sufficient funds", function() {
+        expect(account.withdraw(10)).toEqual("withdrawal successful")
+      })
+
+      it("decreases the balance by the withdrawal amount", function() {
+        account.withdraw(10);
+        expect(account.balance).toEqual(1)
+      })
+
     })
+
   })
+
 
   describe("make a deposit", function() {
 
+    it("on success returns a message confirming the deposit", function() {
+      expect(account.deposit(10)).toEqual("You have successfully deposited 10 into your account")
+    })
+
     it("making a deposit increases the account balance", function() {
-      account.deposit(1000)
+      account.deposit(1000);
       expect(account.balance).toEqual(1000)
+    })
+  })
+
+  describe("printing a statement", function() {
+    it("lists the latest transactions", function() {
+      expect(account.printStatement).toBeDefined()
     })
   })
 
--- jasmine-standalone-2.5.2/src/Account.js
+++ jasmine-standalone-2.5.2/src/Account.js
@@ -2,18 +2,26 @@
 
   function Account() {
     this.balance = 0;
+    this.statement = "Log"
   }
 
     Account.prototype.deposit = function(amount) {
       this.balance+= amount;
+      return "You have successfully deposited 10 into your account";
     }
 
     Account.prototype.withdraw = function(amount) {
       if (amount <= this.balance) {
-        return "withdrawal successful"
+        this.balance-= amount;
+        statement.createWithdrawal()
+        return "withdrawal successful";
       } else {
-        throw Error("You have insufficient funds")
+        throw Error("You have insufficient funds");
       }
+    }
+
+    Account.prototype.printStatement = function() {
+
     }
 
   exports.Account = Account;
